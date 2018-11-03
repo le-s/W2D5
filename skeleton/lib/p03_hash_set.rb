629f39d7ddf0fcd474cfde67934463b88984e9e9
le-s <steven.an.le@gmail.com>
2018-11-02 17:02:25 -0700

    Implement hash set and linked list

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -7,12 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    bucket_index = key.hash % @store.length
+    unless @store[bucket_index].include?(key)
+      @store[bucket_index] << key
+      @count += 1
+      if @count >= @store.length
+        resize!
+      end
+      return true
+    end
+    false
   end
 
   def include?(key)
+    bucket_index = key.hash % @store.length
+    @store[bucket_index].include?(key)
   end
 
   def remove(key)
+    bucket_index = key.hash % @store.length
+    if @store[bucket_index].include?(key)
+      @store[bucket_index].delete(key)
+      @count -= 1
+      return true
+    end
+    false
   end
 
   private
@@ -26,5 +45,13 @@ class HashSet
   end
 
   def resize!
+    result = Array.new(@store.length * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |num|
+        insert_index = num % result.length
+        result[insert_index] << num unless result[insert_index].include?(num)
+      end
+    end
+    @store = result
   end
 end
--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,7 +22,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new(nil, nil)
+    @tail = Node.new(nil, nil)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +37,98 @@ class LinkedList
   end
 
   def first
+    @head.next
+    #byebug
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    search_next = @head.next
+    until search_next.key == key
+      search_next = search_next.next
+      if search_next == @tail
+        return nil
+      end
+    end
+    search_next.val
   end
 
   def include?(key)
+    search_next = @head.next
+    until search_next == @tail
+      if search_next.key == key
+        return true
+      else
+        search_next = search_next.next
+      end
+    end
+
+    false
   end
 
   def append(key, val)
+    last_node = @tail.prev
+    add_node = Node.new(key, val)
+    last_node.next = add_node
+    add_node.prev = last_node
+    add_node.next = @tail
+    @tail.prev = add_node
   end
 
   def update(key, val)
+    # debugger
+    search_next = @head.next
+    if search_next == @tail
+      return nil
+    end
+
+    until search_next.key == key
+      search_next = search_next.next
+      if search_next == @tail
+        return nil
+      end
+    end
+
+    search_next.val = val
+    search_next.val
   end
 
   def remove(key)
+    search_next = @head.next
+    until search_next.key == key
+      search_next = search_next.next
+      if search_next == @tail
+        return nil
+      end
+    end
+
+    node_to_remove = search_next
+    node_to_remove.prev.next = node_to_remove.next
+    node_to_remove.next.prev = node_to_remove.prev
+
   end
 
   def each
+    result = []
+    search_next = @head.next
+
+    until search_next == @tail
+      #byebug
+      yield(search_next)
+      search_next = search_next.next
+    end
+    result
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
